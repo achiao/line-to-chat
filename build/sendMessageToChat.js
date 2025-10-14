8b51373171452e14ff0a0636a1db2f934c80511c
darrenh <darrenh@synology.com>
2025-10-14 14:08:45 +0800

    fix: forget to build code

--- build/sendMessageToChat.js
+++ build/sendMessageToChat.js
@@ -7,7 +7,7 @@ const https_1 = __importDefault(require("https"));
 async function sendMessageToChat(text = '', fileURL = '', chatToken = '') {
     return new Promise((resolve) => {
         let data = '';
-        https_1.default.get(`https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22${chatToken}%22&payload={"text": "${text}", "file_url": "${fileURL}"}`, (res) => {
+        https_1.default.get(`https://chat.synology.com/webapi/entry.cgi?api=SYNO.ChatPlus.External&method=incoming&version=1&token=%22${chatToken}%22&payload={"text": "${text}", "file_url": "${fileURL}"}`, (res) => {
             res.on('data', (chunk) => {
                 data += chunk;
             });
